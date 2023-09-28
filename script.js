c188832a1fdb103b83f411e69008b027fb9dc132
tronit <ttronit@gmail.com>
2023-09-27 21:25:10 -0400

    Update

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-document.getElementById("main").onclick = function() {lomio()};
+function sunControl() {
 
-function lomio() {
-  var precioTrabajo = document.getElementById("precio");
-  var vendedor = precioTrabajo * 10 / 100;
-  precioTrabajo = precioTrabajo - vendedor;
+  let precioTrabajo = document.getElementById("precio").value;
+  let instalador = precioTrabajo * 20 / 100;
+  let numeroInstallers = document.getElementById("instaladores").value;
+  if (numeroInstallers >= 0) {
+    instalador = instalador / numeroInstallers;
+  };
+  let sky = document.getElementById("skylites").value;
+  if (sky > 0){
+    sky = sky * 25 / numeroInstallers;
+    instalador = instalador + sky;
+  };
+  let days = document.getElementById("dias").value;
+  if (days > 1) {
+    days = instalador / days;
+    document.getElementById('main').innerHTML = "Your Estimate Payment is $ " + instalador.toPrecision(5) +
+      " and " +
+      days.toPrecision(5) + " Daily";
+  } else if (days <= 1) {
+    document.getElementById('main').innerHTML = "Your Estimate Payment is $ " + instalador.toPrecision(5);
+  };
+}
 
-  var film = document.getElementById("filma");
-  switch (film) {
-    case film1:
-      var instalador = precioTrabajo * 18 / 100;
-      break;
-    case film2:
-      var instalador = precioTrabajo * 15 / 100;
-      break
-    default:
-      document.getElementById("main").innerHTML = "Defina todo";
+function safety() {
 
+
+  let precioTrabajo = document.getElementById("precio").value;
+  let instalador = precioTrabajo * 20 / 100;
+  let sqf = document.getElementById("pies2").value;
+  sqf = sqf * 7 / 5 * 0.25;
+  instalador = instalador + sqf;
+  let numeroInstallers = document.getElementById("instaladores").value;
+  if (numeroInstallers > 0) {
+    instalador = instalador / numeroInstallers;
+  };
+  let sky = document.getElementById("skylites").value;
+  if (sky => 0){
+    sky = sky * 25 / numeroInstallers;
+    instalador = instalador + sky;
   }
 
-  var installers = document.getElementById("instaladores");
-  if (installers >= 0) {
-      installers = instalador / installers; 
-  }
+  let days = document.getElementById("dias").value;
+  if (days > 1) {
+    days = instalador / days;
+    document.getElementById('main').innerHTML = "Your Estimate Payment is $ " + instalador.toPrecision(5) +
+      " and " +
+      days.toPrecision(5) + " Daily";
+  } else if (days <= 1) {
+    document.getElementById('main').innerHTML = "Your Estimate Payment is $ " + instalador.toPrecision(5);
+  };
 
-
-var diasTrabajo = document.getElementById("dias");
-if (diasTrabajo > 1) {
-    diasTrabajo = instalador / diasTrabajo; 
-    document.getElementById("main").innerHTML = "You gonna be getting " + instalador
-} else if 
-    (diasTrabajo <= 1) {
-        document.getElementById('main').innerHTML = instalador;
-    }
 }
